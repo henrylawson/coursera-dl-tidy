0f1c42c210cf7d730f2eae3dcfe25f512b5c9143
Henry Lawson <henry.lawson@foinq.com>
2013-11-19 13:01:12 -0200

    Changed the script to remove the index from the file name too

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ coursera-dl-tidy
 ================
 
 A yak shaving exercise to play with Ruby and clean up coursera-dl file names.
+
+getting started
+===============
+1. `coursera-dl-tidy/tidy.rb check` to see what it will do
+2. `coursera-dl-tidy/tidy.rb rename` to do it
--- file_tidy.rb
+++ file_tidy.rb
@@ -19,8 +19,12 @@ class FileTidy
 	def filename_as_parent_folder_name(filename)
 		split_filename = filename.split(File::SEPARATOR)
 		filetype = split_filename[-1].split(FILE_EXTENSION_DOT)[-1]
-		split_filename[-1] = split_filename[-2] + FILE_EXTENSION_DOT + filetype
+		split_filename[-1] = remove_index(split_filename[-2]) + FILE_EXTENSION_DOT + filetype
 		split_filename.join(File::SEPARATOR)
+	end
+
+	def remove_index(filename)
+		filename[5..-1]
 	end
 
 end
